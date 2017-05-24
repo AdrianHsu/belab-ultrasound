66554d9390101e0b2aea4a9ecb1279ea768e24d8
AdrianHsu <adrianhsu1995@gmail.com>
2017-05-24 23:56:37 +0800

    done

--- hw3_cnr/hw3_cnr.m
+++ hw3_cnr/hw3_cnr.m
@@ -1,7 +1,7 @@
 clear;
 
 DR=60;
-ENUM = 10; % 1, 4, 7, 10
+ENUM = 7; % 1, 4, 7, 10
 str_enum = ["high_pen_-6-15", "high_pen_+-3", "high_pen_+6+15", ...
             "high_res_-6-15", "high_res_+-3", "high_res_+6+15", ...
             "low_pen_-6-15", "low_pen_+-3", "low_pen_+6+15", ...
@@ -12,6 +12,8 @@ xywh_out = [95 260 15 50];
 
 xywh = [xywh_in;xywh_out];
 table = [];
+fig_hw3_1 = figure();
+set (fig_hw3_1, 'Visible', 'off');
 for i = 1:3
     path = strcat('img/', str_enum(ENUM + i - 1));
     filename = strcat(path, '.bmp');
@@ -20,15 +22,13 @@ for i = 1:3
     GrayIm = double(GrayIm);
     % figure;imagesc(GrayIm);colormap(gray)
 
-
     GrayIm = GrayIm(45:430, 227:425); %-6-15
 
     dBIm = GrayIm - min(min(GrayIm));	% set min value to 0
     dBIm = dBIm/max(max(dBIm));			% normalization, 0 - 1
     dBIm = dBIm*DR;
 
-    fig_hw3_1 = figure();
-    set (fig_hw3_1, 'Visible', 'off');
+    subplot(1,3,i);
     image(dBIm)
     colormap(gray(DR))
     rectangle('Position' , xywh(1,:) , 'Edgecolor' , 'r')
@@ -36,8 +36,7 @@ for i = 1:3
     axis image
     colorbar
     title('B-mode image')
-    name = strcat(str_enum(ENUM + i - 1), '.jpg');
-    saveas(fig_hw3_1, char(name));
+
 
     InIm = dBIm(260:310 , 45:70);
     OutIm = dBIm(140:190 , 100:115);
@@ -50,7 +49,9 @@ for i = 1:3
     CNR = abs((InMean - OutMean)/(InStd + OutStd));  
     table = [table CNR];
 end
+id = uint8(ENUM / 3) + 1;
+name = strcat(name_enum(id), '.jpg');
+saveas(fig_hw3_1, char(name));
 
 csv = array2table(table,'VariableNames', {'A15dBto6dB', 'B3dBto3dB', 'C6dBto15dB'}, 'RowNames', {'CNR'});
-id = uint8(ENUM / 3) + 1;
 writetable(csv, strcat(name_enum(id),'.csv'), 'WriteRowNames', true);
